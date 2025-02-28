a4b82e3160dcd124288a700924efa6d37c5167cf
ahaoboy <504595380@qq.com>
2025-02-28 15:52:51 +0800

    add win

--- build.sh
+++ build.sh
@@ -7,9 +7,17 @@ fi
 
 TARGET=$1
 
+git clone https://github.com/google/bloaty.git bloaty --recursive
+
+cd bloaty
 cmake -B build -S .
 cmake --build build
-mkdir dist
-cp -r ./build/bloaty dist/bloaty
+
+mkdir ../dist
+cp -r ./build/bloaty ../dist/bloaty
+
+cd ..
+ls -lh dist
+
 tar -czf ./bloaty-${TARGET}.tar.gz -C dist .
 ls -l ./bloaty-${TARGET}.tar.gz

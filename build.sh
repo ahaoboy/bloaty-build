de1e367de4169c2cf34153dbfeec603287b0ac20
ahaoboy <504595380@qq.com>
2025-02-28 16:00:28 +0800

    add win

--- build.sh
+++ build.sh
@@ -12,9 +12,11 @@ git clone https://github.com/google/bloaty.git bloaty --recursive
 cd bloaty
 cmake -B build -S .
 cmake --build build
+ls -lh build
 
 mkdir ../dist
-cp -r ./build/bloaty ../dist/bloaty
+cp -r ./build/bloaty ../dist
+cp -r ./build/*libbloaty* ../dist
 
 cd ..
 ls -lh dist
